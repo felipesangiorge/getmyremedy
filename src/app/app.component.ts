2f79ee6c4c8dd89598e41552427eeecde5a34c41
Felipe Sangiorge <felipe.sangiorge@gmail.com>
2018-04-18 15:34:23 -0300

    implementando recursos de design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,56 @@
 import { Component } from '@angular/core';
+import {Router}from'@angular/router'
+import { HostListener} from "@angular/core";
+import {trigger,state,style,transition,animate}from "@angular/animations"
 
 @Component({
   selector: 'gmr-app',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  animations:[
+  trigger('flyInOut', [
+    state('in', style({transform: 'translateX(0)'})),
+    transition('void => *', [
+      style({transform: 'translateX(-3000%)'}),
+      animate(900)
+    ]),
+    transition('* => void', [
+      animate(300, style({transform: 'translateX(100%)'}))
+    ])
+  ])
+ ]
 })
 export class AppComponent {
 
   content= 'Welcome to GetMyRemedy'
-  constructor(){}
+  state:boolean
+  showBackTop:boolean = false
+  flyInOutVisibility = 'in'
+
+  constructor(public router:Router){
+
+  }
 
   ngOnInit(){
 
   }
+
+  @HostListener("window:scroll", ['$event'])
+onWindowScroll() {
+
+  if(window.pageYOffset > 500){
+    this.showBackTop = true
+  }else{
+    this.showBackTop = false
+  }
+}
+
+backToTop(){
+  window.scrollTo(0,0)
+}
+
+toggleFlyIn(){
+  this.flyInOutVisibility = 'in'
+}
+
+
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,20 +1,64 @@
 import { Component, OnInit, Compiler } from '@angular/core';
 import {LoginRegisterService}from'../login-register/login-register.service'
+import {trigger,state,style,transition,animate}from "@angular/animations"
+import { HostListener} from "@angular/core";
 
 @Component({
   selector: 'gmr-home',
-  templateUrl: './home.component.html'
+  templateUrl: './home.component.html',
+  animations: [
+    trigger('popOverStateOne', [
+      state('show', style({
+        opacity: 1
+      })),
+      state('hide',   style({
+        opacity: 0
+      })),
+      transition('show => hide', animate('1000ms ease-out')),
+      transition('hide => show', animate('1000ms ease-in'))
+    ]),
+    trigger('popOverStateTwo', [
+      state('show', style({
+        opacity: 1
+      })),
+      state('hide',   style({
+        opacity: 0
+      })),
+      transition('show => hide', animate('1000ms ease-out')),
+      transition('hide => show', animate('1000ms ease-in'))
+    ]),
+    trigger('popOverStateTree', [
+      state('show', style({
+        opacity: 1
+      })),
+      state('hide',   style({
+        opacity: 0
+      })),
+      transition('show => hide', animate('1000ms ease-out')),
+      transition('hide => show', animate('1000ms ease-in'))
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
+
+    show = 'hide'
+    showTwo='hide'
+    showTree='hide'
+    showBackTop:boolean = false
+
 
   constructor(private loginRegisterService: LoginRegisterService, private compiler:Compiler) { }
 
   ngOnInit() {
+    setTimeout(()=>{ this.show = 'show'}, 500)
+    setTimeout(()=>{ this.showTwo = 'show'}, 1000)
+    setTimeout(()=>{ this.showTree = 'show'}, 1500)
     if(localStorage.getItem('userSessionTokenStorage') != null){
       this.verifyTkr()
     }
         this.compiler.clearCache()
   }
+
 
   verifyTkr(){
 
@@ -24,5 +68,6 @@ export class HomeComponent implements OnInit {
       this.loginRegisterService.logout()
     })
   }
+
 
 }
